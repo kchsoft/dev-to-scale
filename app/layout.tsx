e40725300adadbf4e8ebc206123433b5cbfd2502
kchranger <kchranger@naver.com>
2026-08-27 15:59:51 +0900

    feat: animate requests across the topology

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import type { ReactNode } from "react";
 import "./globals.css";
 import "./request-flow.css";
+import "./topology-map.css";
 import "./dense-ui.css";
 import "./mobile.css";
 
